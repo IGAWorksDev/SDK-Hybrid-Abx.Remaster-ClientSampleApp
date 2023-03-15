a5d69ad8ebadf05d197ac0c6e3409e0833931bfe
bobos-igaw <bobos.son@igaworks.com>
2023-03-15 10:55:17 +0900

    웹페이지 캐시하지 않도록 변경

--- sample/android/app/src/main/java/com/example/williamhybrid/MainActivity.java
+++ sample/android/app/src/main/java/com/example/williamhybrid/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.williamhybrid;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.ConsoleMessage;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -27,9 +29,26 @@ public class MainActivity extends AppCompatActivity {
 
     private void settingWebView(WebView webView){
         // 웹뷰 세팅
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setLoadWithOverviewMode(true);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setSupportZoom(false);
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(false);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webSettings.setPluginState(WebSettings.PluginState.ON);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setSupportMultipleWindows(true);
+        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setAppCacheEnabled(false);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setGeolocationEnabled(false);
+        webSettings.setDefaultTextEncodingName("utf-8");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
         webView.addJavascriptInterface(new AdbrixJavascriptInterface(getApplicationContext()), "adbrixBridge");
         webView.setWebViewClient(new WebViewClient(){});
         webView.setWebChromeClient(new WebChromeClient());
