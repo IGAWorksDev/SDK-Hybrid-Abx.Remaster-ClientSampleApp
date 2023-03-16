60ec0f9f8096feb6b4cda76f8ad538015d0bb38e
Jimmy <jimmy.kang@igaworks.com>
2023-03-16 12:02:58 +0900

    AdbrixJavascriptInterface에 message.body만 전달하도록 수정, 그 외 리팩터링

--- sample/ios/HybridTest/AppDelegate.swift
+++ sample/ios/HybridTest/AppDelegate.swift
@@ -10,7 +10,7 @@ import AdBrixRmKit
 import OSLog
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate, AdBrixRMLogDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate  {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -34,10 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AdBrixRMLogDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-    func didPrintLog(level: AdBrixRM.AdBrixLogLevel, log: String) {
-        os_log(.debug, "%@", log)
     }
 
 }
